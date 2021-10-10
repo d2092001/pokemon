a087cb2ab2d36e469d7a830673b00fc1151d896d
d2092001 <dukai201722@gmail.com>
2021-10-10 16:15:55 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/pokemon/" // Thay tên repository của các bạn vào đây nhé
-      : "/",
+  publicPath: "/pokemon/",
 };
